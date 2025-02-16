9cbf016d0cffacea696f1e8c3602722a978c23f2
Nikita Levin <levin.nikitaa@gmail.com>
2025-02-16 21:33:10 +0300

    feat: Image hover effect for team photos

--- js/script.js
+++ js/script.js
@@ -231,4 +231,23 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     slider();
+
+    //Change image
+    const changeCommandImages = () => {
+        const commandImages = document.querySelectorAll('.command__photo');
+
+        commandImages.forEach(item => {
+            const defaultImage = item.src,
+                hoverImage = item.dataset.img;
+            item.addEventListener('mouseover', () => {
+                item.src = hoverImage;
+            });
+
+            item.addEventListener('mouseout', () => {
+                item.src = defaultImage;
+            });
+        });
+    };
+
+    changeCommandImages();
 });
