58facc38064983070fb638a5080016a46a325423
Nikita Levin <levin.nikitaa@gmail.com>
2025-02-17 12:18:35 +0300

    feat: Input validation for calculator and form fields

--- js/script.js
+++ js/script.js
@@ -250,4 +250,56 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     changeCommandImages();
+
+    //validation
+
+    const inputValidation = () => {
+        //calc-item only numbers
+        const calcInput = document.querySelectorAll('.calc-item');
+        calcInput.forEach(item => {
+            item.addEventListener('input', () => {
+                item.value = item.value.replace(/\D/g, '');
+            });
+        });
+
+        //your name and message only letters
+        const formName = document.querySelector('.form-name'),
+            formEmail = document.querySelector('.form-email');
+        formName.addEventListener('input', () => {
+            formName.value = formName.value.replace(/[^a-zA-Z\s-]+/g, '');
+        });
+        formEmail.addEventListener('input', () => {
+            formEmail.value = formEmail.value.replace(/[^a-zA-Z@\-_.!~*']+$/g, '');
+        });
+
+        //phone number
+        const phoneInput = document.querySelector('.form-phone');
+        phoneInput.addEventListener('input', () => {
+            phoneInput.value = phoneInput.value.replace(/[^0-9()\-+]+/g, '');
+        });
+
+        //have a question block
+        const topFormName = document.querySelectorAll('.top-form'),
+            messageInput = document.querySelector('.mess');
+        topFormName.forEach(item => {
+            if (item.classList.contains('form-email')) {
+                item.addEventListener('input', () => {
+                    item.value = item.value.replace(/[^a-zA-Z@\-_.!~*']+$/g, '');
+                });
+            } else if (item.classList.contains('form-phone')) {
+                item.addEventListener('input', () => {
+                    item.value = item.value.replace(/[^0-9()\-+]+/g, '');
+                });
+            } else {
+                item.addEventListener('input', () => {
+                    item.value = item.value.replace(/[^a-zA-Z\s-]+/g, '');
+                });
+            }
+        });
+        messageInput.addEventListener('input', () => {
+            messageInput.value = messageInput.value.replace(/[^a-zA-Z\s-]+/g, '');
+        });
+    };
+
+    inputValidation();
 });
