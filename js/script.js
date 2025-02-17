107f70f53c1437797b6144ba314111435e7ef33a
Nikita Levin <levin.nikitaa@gmail.com>
2025-02-17 13:07:08 +0300

    added calc

--- js/script.js
+++ js/script.js
@@ -302,4 +302,48 @@ window.addEventListener('DOMContentLoaded', () => {
     };
 
     inputValidation();
+
+    //calculator
+    const calc = (price) => {
+        const calcBlock = document.querySelector('.calc-block'),
+            calcType = document.querySelector('.calc-type'),
+            calcSquare = document.querySelector('.calc-square'),
+            calcDay = document.querySelector('.calc-day'),
+            calcCount = document.querySelector('.calc-count'),
+            totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+                countValue = 1,
+                dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+                squareValue = +calcSquare.value;
+
+            console.log(typeValue);
+            if (calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if (calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if (typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            }
+
+            totalValue.textContent = Math.floor(total);
+        };
+
+        calcBlock.addEventListener('input', (event) => {
+            const target = event.target;
+            if (target.matches('select') || target.matches('input')) {
+                countSum();
+            }
+        });
+    };
+
+    calc(100);
 });
